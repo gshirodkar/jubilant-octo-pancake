12764ec0c71e987cb50ad3f6432bcd671a660ecd
Gaurav Shirodkar <gauravmshirodkar@gmail.com>
2021-03-27 11:48:03 -0400

    first commit

---
+++ xdice.py
@@ -0,0 +1,12 @@
+import random
+
+print ("How many sides do you want? ")
+
+sides=input("How many sides to the die do you want? ")
+
+sidesint=int(sides)
+
+randomnum=random.randrange(1,sides)
+
+
+print ("the number you got is ..." + randomnum)
